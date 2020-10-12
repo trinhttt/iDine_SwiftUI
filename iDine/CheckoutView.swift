c1b0bc6d0e5564ea00ff7dc191e3880aa432c8cf
Thai Thi Tu Trinh <ttttrinh132@gmail.com>
2020-10-13 05:59:06 +0700

    *V10: tow-way bindings

--- iDine/CheckoutView.swift
+++ iDine/CheckoutView.swift
@@ -10,15 +10,39 @@ import SwiftUI
 struct CheckoutView: View {
     @EnvironmentObject var order: Order
     static let paymentTypes = ["Cash", "Creadit Card", "iDine Points"]
+    static let tipAmounts = [10, 15, 20, 25, 0]
     @State private var paymentType = 0
-    
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    @State private var tipAmount = 1
     var body: some View {
-        VStack {
+        Form {
             Section {
-                Picker("How do you want to pay", selection: $paymentType.projectedValue) {
+                Picker("How do you want to pay?", selection: $paymentType) {
                     ForEach(0..<Self.paymentTypes.count) {
                         Text(Self.paymentTypes[$0])
                     }
+                }
+                
+                Toggle(isOn: $addLoyaltyDetails.animation(), label: {
+                    Text("Add iDine loyalty card")
+                })
+                
+                if addLoyaltyDetails {
+                    TextField("Enter ID", text: $loyaltyNumber)
+                }
+            }
+            Section(header: Text("Add a tips")) {
+                Picker("Percentage", selection: $tipAmount) {
+                    ForEach(0..<Self.tipAmounts.count) {
+                        Text("\(Self.tipAmounts[$0])%")
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+            }
+            
+            Section(header: Text("TOTAL: $100")) {
+                Button("Confirm order") {
+                    
                 }
             }
         }
--- iDine/OrderView.swift
+++ iDine/OrderView.swift
@@ -23,7 +23,7 @@ struct OrderView: View {
                 }
                 Section {
                     NavigationLink(
-                        destination: Text("Check out")) {
+                        destination: CheckoutView()) {
                         Text("Place Order")
                     }
                 }
